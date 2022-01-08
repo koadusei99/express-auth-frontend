64b8078eea68f7c8a002cff839f41c02711d16cc
Maampeaw <markamoah97@gmail.com>
2022-01-08 16:19:16 +0000

    login vue and reset password vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,121 @@
 <script></script>
 
 <template>
-  <h1>LOGIN</h1>
+  <div class="app">
+
+    <div class="app-container">
+      <div class="logo"><img src="../assets/logo.svg" alt=""></div>
+
+      <div class="content-wrapper">
+        <div class="content-left">
+          <div class="log">Log in</div>
+          <div class="welcome-note">Great to have you back</div>
+        </div>
+
+
+        <div class="content-right white-card">
+          <form action="" class="login">
+            <div class="input-wrapper">
+            <div class='label'><label for="mail">email address</label></div>
+            <input type="email" class="email" name="mail">
+            </div>
+            
+            <div class="input-wrapper">
+             <div class='label'><label for="password">password</label></div>
+            <input type="password" class="pass" name="password">
+            </div>
+
+            <div class="forgot-password">forgot password</div>
+
+            <button class="login-btn">Login</button>
+
+            <div class="final"><span class='account'>Don't have an account?</span> <router-link to="/register" class='now'>Register now</router-link></div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.app-container{
+  margin: 80px 140px;
+}
+
+.content-wrapper{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 100px;
+  
+}
+.log{
+  text-transform: capitalize;
+  font-weight: bold;
+  font-size: 46px;
+  line-height: 48px;
+  color: var(--text-dark);
+  margin-top: 50px;
+}
+.welcome-note{
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 42px;
+  margin-top: 20px;
+  color: var(--gray3)
+}
+
+.content-right{
+  padding: 32px;
+  border-radius: 8px;
+  width: 30vw;
+ 
+}
+.label{
+  text-transform: capitalize;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 0.4px;
+  color: var(--gray2);
+  
+}
+
+.input-wrapper{
+  margin-bottom: 24px;
+}
+.login-btn{
+  margin-top: 24px;
+  width: 100%;
+}
+.forgot-password{
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  color: var(--primary);
+}
+.login-btn{
+  background: var(--primary);
+  border-radius: 8px;
+  padding: 12px 0;
+  color: white;
+}
+input{
+  border: 1px solid #DEE3EB;
+  height: 40px;
+  border-radius: 4px;
+  width: 100%;
+}
+.label{
+  margin-bottom: 8px;
+}
+.final{
+  margin-top: 24px;
+  font-size: 14px;
+  line-height: 16px;
+  color: var(--gray2);
+}
+.final .now{
+  color: var(--primary)
+
+}
+</style>
--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,7 +1,67 @@
 <script></script>
 
 <template>
-  <h1>Reset Password</h1>
+ <div class="relative forgot my-[80px] mx-[80px]">
+    <div><img src="../assets/logo.svg" alt="logo" class="ml-1 w-[37px]" /></div>
+    <div class="content my-[100px] flex justify-between">
+      <div class="txt mt-[50px]">
+        <h2 class="text-[46px] font-bold text-gray-300">Reset Password</h2>
+        <p class="text-gray-100 text-3xl max-w-[480px] mt-6">
+         Create a unique password to keep your account protected
+        </p>
+      </div>
+      <div class="max-w-[500px] w-full">
+        <Form
+          class="white-card flex flex-col w-full px-8 py-10 rounded-lg shadow-sm"
+          action=""
+          @submit="onSubmit"
+        >
+          <label class="text-sm text-gray-200 mb-2" for="email"
+            >New Password</label
+          >
+          <Field
+            class="border p-4 text-gray-300 border-bdr rounded h-12 ring-offset-2 hover:border-gray-100 focus:ring-2 focus:ring-primary-100 focus:border-primary-200 focus:ring-opacity-30"
+            id="email"
+            :rules="emailVal"
+            name="email"
+            v-model="email"
+          />
+
+
+           <label class="mt-4 text-sm text-gray-200 mb-2" for="email"
+            >Confirm Password</label
+          >
+          <Field
+            class=" border p-4 text-gray-300 border-bdr rounded h-12 ring-offset-2 hover:border-gray-100 focus:ring-2 focus:ring-primary-100 focus:border-primary-200 focus:ring-opacity-30"
+            id="email"
+            :rules="emailVal"
+            name="email"
+            v-model="email"
+          />
+
+          <ErrorMessage name="email" class="text-red-600 text-xs py-2" />
+          <button
+            :disabled="busy"
+            type="submit"
+            class="disabled:bg-prim-disabled disabled:cursor-wait relative bg-primary-200 rounded h-12 shadow-sm hover:shadow-md hover:bg-primary-100 hover:-translate-y-[2px] text-white font-semibold mt-6 transition"
+          >
+            Reset Password
+            <span
+              :class="`${
+                busy ? 'show' : 'hidden'
+              } w-5 h-5 bg-white absolute right-3 top-3 rounded-full animate-ping`"
+            ></span>
+          </button>
+
+          <router-link
+            class="text-primary-200 text-sm mt-6 text-center font-semibold"
+            :to="{ name: 'Login' }"
+            >Return to Log In</router-link
+          >
+        </Form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
